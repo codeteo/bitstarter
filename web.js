4ae10e0c3542ff763f6d227943649d60a4d4edc7
codeteo <ramones.teo@gmail.com>
2013-07-11 23:36:25 +0300

    testing

--- web.js
+++ web.js
@@ -1,10 +1,13 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
 
 app.get('/', function(request, response) {
-  //response.send('Hello World!');
-  response.send('Hello World 2!');
+//	var buffer = new Buffe(100);
+//	output = fs.readFileSync('index.html').toString();
+//	response.send(output);
+	response.send('test test');
 });
 
 var port = process.env.PORT || 5000;
